7e04401a51d7c43dc5710ff97df79126c88ca2f8
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-06 02:37:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears ('Mr. Chocolate', 1, 'M', 'brown', 'calm', 'y');
 INSERT INTO bears ('Rowdy', 1, 'M', 'black', 'hyper', 'y');
-INSERT INTO bears ('Tabitha')
-INSERT INTO bears ('Sergeant Brown')
-INSERT INTO bears ('Melissa')
-INSERT INTO bears ('Grinch')
-INSERT INTO bears ('Wendy')
-INSERT INTO bears (NULL)
+INSERT INTO bears ('Tabitha', 2, 'F', 'brown', 'calm', 'y');
+INSERT INTO bears ('Sergeant Brown', 2, 'M', 'black', 'grumpy', 'y');
+INSERT INTO bears ('Melissa', 3, 'F', 'brown', 'calm', 'y');
+INSERT INTO bears ('Grinch', 3, 'F', 'black', 'grumpy', 'y');
+INSERT INTO bears ('Wendy', 4, 'F', 'brown', 'calm', 'y');
+INSERT INTO bears (NULL, 4, 'M', 'black', 'grumpy', 'n');
