39873429daa2c9150afc3af060d10c2a50012425
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-06 02:41:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears ('Mr. Chocolate', 1, 'M', 'brown', 'calm', 'y');
-INSERT INTO bears ('Rowdy', 1, 'M', 'black', 'hyper', 'y');
-INSERT INTO bears ('Tabitha', 2, 'F', 'brown', 'calm', 'y');
-INSERT INTO bears ('Sergeant Brown', 2, 'M', 'black', 'grumpy', 'y');
-INSERT INTO bears ('Melissa', 3, 'F', 'brown', 'calm', 'y');
-INSERT INTO bears ('Grinch', 3, 'F', 'black', 'grumpy', 'y');
-INSERT INTO bears ('Wendy', 4, 'F', 'brown', 'calm', 'y');
-INSERT INTO bears (NULL, 4, 'M', 'black', 'grumpy', 'n');
+INSERT INTO bears VALUES ('Mr. Chocolate', 1, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES ('Rowdy', 1, 'M', 'black', 'hyper', 1);
+INSERT INTO bears VALUES ('Tabitha', 2, 'F', 'brown', 'calm', 1);
+INSERT INTO bears VALUES ('Sergeant Brown', 2, 'M', 'black', 'grumpy', 1);
+INSERT INTO bears VALUES ('Melissa', 3, 'F', 'brown', 'calm', 1);
+INSERT INTO bears VALUES ('Grinch', 3, 'F', 'black', 'grumpy', 1);
+INSERT INTO bears VALUES ('Wendy', 4, 'F', 'brown', 'calm', 1);
+INSERT INTO bears VALUES (NULL, 4, 'M', 'black', 'grumpy', 0);
