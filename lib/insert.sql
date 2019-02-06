383f8c1d0c2f774ab16ca2eafec1e6759a23234a
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-06 02:35:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,9 @@
-INSERT INTO bears ('Mr. Chocolate', 1, 'M', 'brown', 'calm', 'y')
+INSERT INTO bears ('Mr. Chocolate', 1, 'M', 'brown', 'calm', 'y');
+INSERT INTO bears ('Rowdy', 1, 'M', 'black', 'hyper', 'y');
+INSERT INTO bears ('Tabitha')
+INSERT INTO bears ('Sergeant Brown')
+INSERT INTO bears ('Melissa')
+INSERT INTO bears ('Grinch')
+INSERT INTO bears ('Wendy')
+INSERT INTO bears (NULL)
+
